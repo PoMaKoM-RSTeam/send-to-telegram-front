1f8745c97a6bddf4f76a2dd5afd0ba575094e09b
Ekaterina Kachan <eka.kachan@gmail.com>
2022-01-09 19:45:28 +0300

    feat: add actions eslit prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="wrapper">
-      <h1>Telegram Bot</h1>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <h1>Telegram Bot</h1>
+        </div>
+    );
 }
 
 export default App;
