5fa4e29755922f78a5956e5592f63f26a787b183
Ekaterina Kachan <eka.kachan@gmail.com>
2022-01-17 15:03:43 +0300

    fix: fix prittier

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/style.css';
+import './css-dist/style.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
