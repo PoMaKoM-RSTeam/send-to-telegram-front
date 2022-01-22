41a8758e0c6bf9ac6d5437cb6bf8c08c5a88d73a
Ekaterina <70857908+KaterinaKachann@users.noreply.github.com>
2022-01-22 16:41:40 +0300

    Feature/s1/fr 32/add routing (#26)
    * feat: add routing
    
    * fix: fix prettier problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './scss/style.scss';
+import './scss/main.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
---
+++ src/pages/login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../scss/login.scss';
+
+function Login() {
+  return <div> Login Page</div>;
+}
+
+export default Login;
