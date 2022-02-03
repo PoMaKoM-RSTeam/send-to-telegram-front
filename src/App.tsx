2e89d2ce3668149528e1df68a2f5a72e72df7fcd
gomunkool <pivzz19881988@gmil.com>
2022-02-04 01:17:48 +0300

    feat: react guard login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ReactNotifications } from 'react-notifications-component';
 import Login from './pages/Login/Login';
 import Error from './pages/error';
 import Main from './pages/Main/Main';
 import Moderation from './pages/Moderation/Moderation';
+
+interface IRequireAuth {
+  children: JSX.Element;
+  redirectTo: string;
+}
+
+function RequireAuth({ children, redirectTo }: IRequireAuth) {
+  function getAuth() {
+    const userAutorotation = false;
+    return userAutorotation;
+  }
+  const isAuthenticated = getAuth();
+  return isAuthenticated ? children : <Navigate to={redirectTo} />;
+}
 
 function App() {
   return (
@@ -12,7 +26,14 @@ function App() {
       <ReactNotifications />
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/*" element={<Main />} />
+        <Route
+          path="/*"
+          element={(
+            <RequireAuth redirectTo="/login">
+              <Main />
+            </RequireAuth>
+          )}
+        />
         <Route path="*" element={<Error />} />
         <Route path="/Moderation" element={<Moderation />} />
       </Routes>
