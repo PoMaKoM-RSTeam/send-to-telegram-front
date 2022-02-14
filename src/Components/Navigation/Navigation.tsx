0710a7a3cc9123aedb67c2897ce13bf27ad58c36
Ekaterina <70857908+KaterinaKachann@users.noreply.github.com>
2022-02-14 18:12:21 +0300

    change: change layout for navigation menu (#36)
    * change: change layout for navigation menu
    
    * fix: font style and add transition
    
    * fix: fix name channel
    
    * fix: fix eslint and prettier

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import PageUser from '../PageUser/PageUser';
 import './Navigation.scss';
 
 function Navigation() {
+  const [active, setActive] = useState('');
+  useEffect(() => {
+    if (active) {
+      document.body.classList.add('bodyRemoveScroll');
+    } else {
+      document.body.classList.remove('bodyRemoveScroll');
+    }
+  }, [active]);
+  const onSetActiveMenuItem = (item: React.SetStateAction<string>) => {
+    if (item !== active) {
+      setActive(item);
+    } else {
+      setActive(''); // handle click on currently active item
+    }
+  };
   return (
     <aside className="navigation">
-      <div className="logo">RSClon</div>
+      <div className="loginNavigationLogo">
+        <span>RS-Clone</span>
+        <span>Bot</span>
+      </div>
       <PageUser />
       <nav>
         <ul className="navigation__list">
           <li className="navigation__item">
-            <NavLink to="/console">Консоль </NavLink>
+            <NavLink
+              to="/console"
+              onClick={() => onSetActiveMenuItem('item1')}
+              className={active === 'active' ? 'active' : ''}
+            >
+              Консоль{' '}
+            </NavLink>
           </li>
-          <li className="navigation__item">Каналы</li>
+          <li className="navigation__item">
+            <NavLink to="/canals">Каналы</NavLink>
+          </li>
           <li className="navigation__item">
             <NavLink to="/content-plan">Контент-план </NavLink>
           </li>
-          <li className="navigation__item">Статистика</li>
+          <li className="navigation__item">
+            <NavLink to="/stat">Статистика</NavLink>
+          </li>
           <li className="navigation__item">
             <NavLink to="/moderation">Модерация </NavLink>
           </li>
