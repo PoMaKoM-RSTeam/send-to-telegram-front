da86ec3ded84a42a24b64cf94630d84f70990fb7
gomunkool <pivzz19881988@gmil.com>
2022-02-16 01:17:55 +0300

    add: victory library, change schedule

--- src/Pages/StatisticsPage/StatisticsPage.tsx
+++ src/Pages/StatisticsPage/StatisticsPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './StatisticsPage.scss';
+import { VictoryChart, VictoryLine, VictoryVoronoiContainer, VictoryGroup, VictoryScatter } from 'victory';
 import StatisticsButton from '../../Components/UI/StatisticsButton/StatisticsButton';
 
 interface StatisticsButtonsItem {
@@ -15,10 +16,44 @@ export default function StatisticsPage(): JSX.Element {
     { name: 'просмотры', id: 3, content: 'График просмотры' },
   ];
 
+  const dataStatistcs: {
+    x: number;
+    y: number;
+  }[][] = [
+    [
+      { x: 1, y: -3 },
+      { x: 2, y: 10 },
+      { x: 3, y: 3 },
+      { x: 4, y: 0 },
+      { x: 5, y: -2 },
+      { x: 6, y: -2 },
+      { x: 7, y: 5 },
+    ],
+    [
+      { x: 1, y: 3 },
+      { x: 2, y: 0 },
+      { x: 3, y: 4 },
+      { x: 4, y: 10 },
+      { x: 5, y: -4 },
+      { x: 6, y: -2 },
+      { x: 7, y: 1 },
+    ],
+    [
+      { x: 1, y: 0 },
+      { x: 2, y: 2 },
+      { x: 3, y: 5 },
+      { x: 4, y: 7 },
+      { x: 5, y: 4 },
+      { x: 6, y: 1 },
+      { x: 7, y: -6 },
+    ],
+  ];
+
+  const [dataStatistcsToRender, setDataStatistcsToRender] = useState(dataStatistcs[0]);
   const [scheduleContent, setScheduleContent] = useState(statisticsButtons[0]);
 
   const changeSchedule = (value: StatisticsButtonsItem) => {
-    console.log(value);
+    setDataStatistcsToRender(dataStatistcs[value.id - 1]);
     setScheduleContent(value);
   };
 
@@ -38,7 +73,15 @@ export default function StatisticsPage(): JSX.Element {
         ))}
       </ul>
       <div className="schedule-wrapper">
-        <div className="schedule">{content}</div>
+        <h2 className="schedule-title"> {content}</h2>
+        <div className="schedule">
+          <VictoryChart height={500} width={800} containerComponent={<VictoryVoronoiContainer />}>
+            <VictoryGroup color="#c43a31" labels={({ datum }) => `y: ${datum.y}`} data={dataStatistcsToRender}>
+              <VictoryLine />
+              <VictoryScatter size={({ active }) => (active ? 8 : 3)} />
+            </VictoryGroup>
+          </VictoryChart>
+        </div>
       </div>
     </div>
   );
